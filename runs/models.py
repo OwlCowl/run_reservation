6c4c14f48b47f6d539f3fc202f1ea3cbc6e24891
YanaAfanaseva <2947468@gmail.com>
2021-03-19 18:08:34 +0300

    without_relationship

--- runs/models.py
+++ runs/models.py
@@ -2,55 +2,79 @@ from django.db import models
 
 
 # Create your models here.
-'''
-    Here will be the list with whole runs
-'''
-class Main(models.Model):
+# ''' AllRuns - All accessable runs '''
+class AllRuns(models.Model):
     name = models.CharField(max_length=25)
     city = models.CharField(max_length=25)
     distance = models.DecimalField(max_digits=5, decimal_places=2)
     registration = models.BooleanField(default=True)
     term = models.DateField()
 
-    def get_detail_url(self):
-        return f"/main/{self.id}"
 
+    def get_detail_url(self):
+        return f"/AllRuns/{self.id}"
+
+
+
+# ''' Details about specific run '''
+class RunDetails(models.Model):
+    run_name = models.CharField(max_length=64)
+    run_distance = models.IntegerField()
+    run_date = models.DateField()
+    run_participants = models.IntegerField()
+
+
+# ''' Registration - Base with info about participants and payments '''
+class Registration(models.Model):
+    name_pr = models.CharField(max_length=25)
+    surname_pr = models.CharField(max_length=25)
+    date_of_birth = models.DateField()
+    email = models.CharField(max_length=25)
+    gender = models.CharField(max_length=25)
+    phone = models.IntegerField()
+    payment = models.BooleanField(default=False)
+    participants = models.ManyToManyField(AllRuns)
+
+
+    def get_detail_url(self):
+        return f"/Registration/{self.id}"
+
+
+
+
+# ''' Something trial'''
 class RunReservation(models.Model):
-    run_id = models.IntegerField()
     date = models.DateField()
     comment = models.TextField(null=True)
 
-# class DistanceOptions(models.Model):
-#     small = models.IntegerField()
-#     medium = models.IntegerField()
-#     marathon = models.IntegerField()
-#     ultra = models.IntegerField()
 
-# class Payment(models.Model):
-#     price_s = models.DecimalField(max_digits=5, decimal_places=2)
-#     price_m = models.DecimalField(max_digits=5, decimal_places=2)
-#     price_l = models.DecimalField(max_digits=5, decimal_places=2)
-#     price_u = models.DecimalField(max_digits=5, decimal_places=2)
+# ''' Payment - Base with payment info'''
+class Payment(models.Model):
+    status = models.BooleanField(default=False)
+    # run_name = models.ForeignKey(AllRuns, on_delete=models.CASCADE)
 
 
-
-
-class RunEnroll(models.Model):
-    run_enroll_id = models.ForeignKey(Main, on_delete=models.CASCADE)
-    name_pr = models.CharField(max_length=25)
-    surname_pr = models.CharField(max_length=25)
-    date_of_birth_pr = models.DateField()
-    email = models.CharField(max_length=25)
-    gender = models.CharField(max_length=25)
-    number = models.IntegerField()
-
-    def get_detail_url(self):
-        return f"/runenroll/{self.id}"
-
-
-class UserData(models.Model):
+# ''' User private account base: all about his booked runs '''
+class UserPrivateData(models.Model):
     run_name = models.CharField(max_length=25)
     run_city = models.CharField(max_length=25)
     run_date = models.DateField()
     run_distance = models.DecimalField(max_digits=5, decimal_places=2)
-    payment = models.BooleanField(default = False)
+    # payment = models.ForeignKey(Payment, on_delete = models.CASCADE)
+
+# ''' AllRuns - All accessable runs'''
+# ''' UserPrivateData - User private account base: all about his booked runs'''
+# ''' RunDetails - Details about specific run'''
+# ''' Registration - Base with info about participants and payments'''
+# ''' Payment - Base with payment info'''
+''' Participants where we store all users who enrolled'''
+''' start_number '''
+
+
+
+
+
+
+
+
+
--- runs/views.py
+++ runs/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from runs.models import Main, RunEnroll
+from runs.models import AllRuns, Registration
 
 # Create your views here.
 class RunsListView(View):
     def get(self, request):
-        runs = Main.objects.all()
+        runs = AllRuns.objects.all()
         return render(request, 'landing.html', {'runs_list':runs})
 
 class AddRunView(View):
@@ -18,33 +18,33 @@ class AddRunView(View):
         distance = request.POST.get("distance")
         registration = request.POST.get("registration")
         term = request.POST.get("term")
-        Main.objects.create(name=name,city=city,distance=distance, registration=registration, term=term)
+        AllRuns.objects.create(name=name,city=city,distance=distance, registration=registration, term=term)
         return redirect("run_list")
 
 class RunDetailsView(View):
     def get(self, request, run_id):
-        specified_run = Main.objects.get(id=run_id)
+        specified_run = AllRuns.objects.get(id=run_id)
         return render(request, "details_about_run.html", {'run': specified_run})
 
 class RunDelete(View):
     def get(selfself, request, run_id):
-        specified_run = Main.objects.get(id=run_id)
+        specified_run = AllRuns.objects.get(id=run_id)
         specified_run.delete()
         return redirect("run_list")
 
 class RunEdit(View):
     def get(self, request, run_id):
-        specified_run = Main.objects.get(id=run_id)
+        specified_run = AllRuns.objects.get(id=run_id)
         return render(request, "modify_run.html")
 
     def post(self, request, run_id):
-        specified_run = Main.objects.get(id=run_id)
+        specified_run = AllRuns.objects.get(id=run_id)
         name = request.POST.get("name")
         city = request.POST.get("city")
         distance = request.POST.get("distance")
         registration = request.POST.get("registration")
         term = request.POST.get("term")
-        Main.objects.create(name=name,city=city,distance=distance, registration=registration, term=term)
+        AllRuns.objects.create(name=name,city=city,distance=distance, registration=registration, term=term)
         return redirect("run_list")
 
 
@@ -62,30 +62,30 @@ relation many to one
 table has data of enrolled people
 
 '''
-class RunEnrollView(View):
+class RegistrationView(View):
     def get(self, request, run_id):
-        specified_run = Main.objects.get(id=run_id)
+        specified_run = AllRuns.objects.get(id=run_id)
         return render(request, "enroll_form.html")
 
     def post(self, request, run_id):
-        specified_run = Main.objects.get(id=run_id)
+        specified_run = AllRuns.objects.get(id=run_id)
         name_pr = request.POST.get("name_pr")
         surname_pr = request.POST.get("surname_pr")
-        date_of_birth_pr= request.POST.get("date_of_birth_pr")
+        date_of_birth= request.POST.get("date_of_birth")
         email = request.POST.get("email")
         gender = request.POST.get("gender")
-        number= request.POST.get("number")
-        RunEnroll.objects.create(run_enroll_id = specified_run, name_pr=name_pr, surname_pr=surname_pr,date_of_birth_pr=date_of_birth_pr,
-                                 email=email, gender=gender,number=number)
+        phone= request.POST.get("phone")
+        Registration.objects.create(run_enroll_id = specified_run, name_pr=name_pr, surname_pr=surname_pr,date_of_birth=date_of_birth,
+                                 email=email, gender=gender,phone=phone)
 
         return redirect("run_list")
 
 '''
 here should be data of enrolled person runs'''
-class UserDataView(View):
+class UserPrivateDataView(View):
 
     def get(self, request):
-        run_list= RunEnroll.objects.all()
+        run_list= Registration.objects.all()
         return render(request, 'user_account.html', {'run_list':run_list})
 
 
